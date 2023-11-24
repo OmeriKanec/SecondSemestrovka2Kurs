31e21f13ae8a66c173d605d731b89ff96e692267
Lazarev <egor0505-lazarev@mail.ru>
2023-11-24 19:01:41 +0300

    DataBaseConnection, Player model, DAO and DAO implementation for player added

---
+++ src/java/dao/DAO.java
@@ -0,0 +1,12 @@
+package java.dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+    void create(T u);
+    T get(long id);
+    void update(T u);
+    void delete(long id);
+    List<T> getAll();
+}
+
---
+++ src/java/models/Player.java
@@ -0,0 +1,71 @@
+package java.models;
+
+
+public class Player {
+    private long id;
+    private String userName;
+    private String password;
+    private int money;
+    private int avatarFaceId;
+    private int avatarSkinId;
+
+    public Player(long id, String userName, String password, int money, int avatarFaceId, int avatarSkinId) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.money = money;
+        this.avatarFaceId = avatarFaceId;
+        this.avatarSkinId = avatarSkinId;
+    }
+
+    public Player(String userName, String password, int avatarFaceId, int avatarSkinId) {
+        this.userName = userName;
+        this.password = password;
+        this.avatarFaceId = avatarFaceId;
+        this.avatarSkinId = avatarSkinId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getAvatarFaceId() {
+        return avatarFaceId;
+    }
+
+    public void setAvatarFaceId(int avatarFaceId) {
+        this.avatarFaceId = avatarFaceId;
+    }
+
+    public int getAvatarSkinId() {
+        return avatarSkinId;
+    }
+
+    public void setAvatarSkinId(int avatarSkinId) {
+        this.avatarSkinId = avatarSkinId;
+    }
+}
