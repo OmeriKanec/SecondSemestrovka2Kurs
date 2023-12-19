01857d061a457efa320149ea29ab0436f3834511
Lazarev <egor0505-lazarev@mail.ru>
2023-12-19 13:24:29 +0300

    Lobby creation almoust works now

--- src/FxControllers/LobbyCreationController.java
+++ src/FxControllers/LobbyCreationController.java
@@ -3,8 +3,11 @@ package src.FxControllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import src.helpers.ConnectionSingletone;
+import src.models.Room;
 
 public class LobbyCreationController {
     @FXML
@@ -30,5 +33,17 @@ public class LobbyCreationController {
                     bet.setText(newValue.replaceAll("[^\\d]", ""));
                 }
         });
+    }
+
+    public void createNewLobby() {
+        int deathsBeforeFinish;
+        if (deaths.getValue().equals("До первой смерти")){
+            deathsBeforeFinish = 1;
+        } else {
+            deathsBeforeFinish = maxPlayers.getValue() - 1;
+        }
+        Room room = new Room(name.getText(), maxPlayers.getValue(), 1, Integer.parseInt(bet.textProperty().getValue()),
+                bulletCount.getValue(), deathsBeforeFinish);
+        ConnectionSingletone.getConnection().sendNewRoomToServer(room);
     }
 }
