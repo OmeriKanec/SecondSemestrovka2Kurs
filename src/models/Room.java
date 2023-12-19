86e7222dbb772fc5c407ab53abcc304aa98a1fbb
Lazarev <egor0505-lazarev@mail.ru>
2023-12-19 13:25:54 +0300

    Changed Room class in client

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,12 +1,15 @@
 package src.models;
 
+import java.util.UUID;
+
 public class Room {
-    String name;
-    int maxPlayers;
-    int currentPlayers;
-    int bet;
-    int bulletsCount;
-    int deathsBeforeFinish;
+    private String name;
+    private int maxPlayers;
+    private int currentPlayers;
+    private int bet;
+    private int bulletsCount;
+    private int deathsBeforeFinish;
+    private UUID uuid;
 
     public Room(String name, int maxPlayers, int currentPlayers, int bet, int bulletsCount, int deathsBeforeFinish) {
         this.name = name;
@@ -15,5 +18,43 @@ public class Room {
         this.bet = bet;
         this.bulletsCount = bulletsCount;
         this.deathsBeforeFinish = deathsBeforeFinish;
+    }
+
+    public Room(String name, int maxPlayers, int currentPlayers, int bet, int bulletsCount, int deathsBeforeFinish, UUID uuid) {
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+        this.currentPlayers = currentPlayers;
+        this.bet = bet;
+        this.bulletsCount = bulletsCount;
+        this.deathsBeforeFinish = deathsBeforeFinish;
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public int getCurrentPlayers() {
+        return currentPlayers;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public int getBulletsCount() {
+        return bulletsCount;
+    }
+
+    public int getDeathsBeforeFinish() {
+        return deathsBeforeFinish;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 }
